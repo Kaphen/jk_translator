04bee0c5c6a2038875c89f765f43c535660d04bf
huangkaifen <huangkaifen@lizhi.fm>
2024-09-03 14:55:37 +0800

    fix: 修复requested URL was not found问题

--- run_server.py
+++ run_server.py
@@ -6,9 +6,9 @@ from server.gui import GUI
 
 app = Flask(__name__)
 app.register_blueprint(bp)
+gui = GUI()
+app.add_url_rule('/', 'webio_view', webio_view(gui.gui_index),
+                     methods=['GET', 'POST', 'OPTIONS'])
 
 if __name__ == '__main__':
-    gui = GUI()
-    app.add_url_rule('/', 'webio_view', webio_view(gui.gui_index),
-                     methods=['GET', 'POST', 'OPTIONS'])
     app.run(port=8080, debug=True)
